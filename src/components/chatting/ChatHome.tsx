2dfc105eb7447a346d397587fc1e59744609e03b
caminobelllo <alstpgml0812@gmail.com>
2023-11-09 13:58:52 +0900

    Feat: chathome 생성

---
+++ src/components/chatting/ChatHome.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Home = styled.div`
+    height: 1080px;
+    background: var(--Grey-200, #f2f4f6);
+`;
+
+const ChatHome = () => {
+    return <Home></Home>;
+};
+
+export default ChatHome;
